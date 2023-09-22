a45c1181d3b2aea4eecbba81f49db1616ed01816
Jalmer13 <mrjazilla@gmail.com>
2023-09-22 21:36:51 +0300

    1st commit

---
+++ firstapp/Homework.java
@@ -0,0 +1,21 @@
+package firstapp;
+
+public class Homework {
+    public static void main(String[] args) {
+        // Task 1
+        System.out.println("2nd Home Work");
+        // Task 2
+        // Celsius = 5/9 (F - 32) Kelvin = C + 273.16
+        int c = 13;
+        double k = c + 273.16;
+        float f = ((float) c * 9 / 5) + 32;
+        System.out.println(c + " Celsius " + f + " Fahrenheit " + k + " Kelvin ");
+        // Task 3
+        int cateta = 12;
+        int catetb = 16;
+        int hypotenuse2 = ((cateta * cateta) + (catetb * catetb)) ;
+        double hypotenuse = Math.sqrt(hypotenuse2);
+        double s = (float)1/2 * cateta * cateta;
+        System.out.println( s + " area of a right triangle " + hypotenuse + " Hypotenuse ");
+    }
+}
