cb3f21ecc8fe8d8cbe940706a6855574037cf652
Jalmer13 <mrjazilla@gmail.com>
2023-10-06 00:37:12 +0300

    1st commit Homework5

--- HomeWork5/task3.java
+++ HomeWork5/task3.java
@@ -1,24 +1,16 @@
 package HomeWork5;
-
 import java.util.Random;
-
 public class task3 {
-    public task3() {
-    }
-
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         int[] arrayTrd = new int[10];
         Random rnd = new Random();
-
         for(int i = 0; i < arrayTrd.length; ++i) {
             arrayTrd[i] = rnd.nextInt(100);
             double result = (double)(arrayTrd[i] % 2);
             if (result == 0.0) {
                 arrayTrd[i] = 0;
             }
-
             System.out.println(arrayTrd[i]);
         }
-
     }
 }
